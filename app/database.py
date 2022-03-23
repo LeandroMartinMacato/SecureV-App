00f70772eb7659fb01a131d9f8b3874629108deb
Alejandro <shadowslayerhero@gmail.com>
2022-03-23 18:35:14 +0800

    feat: imp Entry DB class

--- app/database.py
+++ app/database.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import datetime as dt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/vehicle_db.sqlite3' # Config to use sqlalchemy
@@ -10,9 +11,10 @@ db = SQLAlchemy(app)
 
 class Vehicle(db.Model):
     id = db.Column("id" , db.Integer , primary_key=True)
-    plate_num = db.Column("plate_num" , db.String(6) )
-    owner_name = db.Column("owner_name" , db.String(20) )
-    date_registered = db.Column("date_registered" , db.Date(), default=datetime.utcnow )
+    plate_num = db.Column("plate_num" , db.String(6), unique=True , nullable=False )
+    owner_name = db.Column("owner_name" , db.String(20) , unique=True , nullable=False)
+    date_registered = db.Column("date_registered" , db.Date(),nullable = False, default=datetime.utcnow )
+    entries = db.relationship('Entry', backref = 'entry_plate' , lazy = True)
 
     def __init__(self, plate_num , owner_name):
         self.plate_num = plate_num
@@ -22,27 +24,34 @@ class Vehicle(db.Model):
     def __repr__(self):
         return f'{self.plate_num}|{self.owner_name}|{self.date_registered}'
 
-class Entries(db.Model):
+class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date_of_entry = db.Column("date_of_entry" , db.Date() , default=datetime.utcnow)
+    date_of_entry = db.Column("date_of_entry" , db.String(30) ,nullable = False , default=dt.datetime.now)
+    # entry_vehicle_id = db.Column(db.Integer, db.ForeignKey("vehicle.id") , nullable = False)
+    entry_vehicle_plate = db.Column(db.String(6), db.ForeignKey("vehicle.plate_num") , nullable = False) # Test
     # vehicle relation here
 
-    def __init__(self):
-        self.date_of_entry = self.date_of_entry
+    # def __init__(self , ):
+    #     self.date_of_entry = self.date_of_entry
+    #     self.vehicle_id = ""
 
     def __repr__(self):
-        return f"Date of Entry: {self.date_of_entry}"
+        return f"{self.date_of_entry}|{self.entry_vehicle_plate}"
 
 class DB_Manager():
     def __init__(self):
         self.db_connected = False
         self.db_data = []
+        self.db_data_entries = []
         self.get_db_data()
         self.count = 0
         count = 0
 
     def get_db_data(self):
         self.db_data = [] # Clear current db_data to get fresh fetch
+        self.db_data_entries = [] # for LOGS
+
+        # Vehicle
         all_db_data = Vehicle.query.all() 
         self.count = 0
         for data in all_db_data:
@@ -55,6 +64,19 @@ class DB_Manager():
                 "date_registered": data[2]
             }
             self.db_data.append(data_breakdown)
+        
+        # Entries
+        all_db_entries = Entry.query.all()
+        self.count = 0
+        for data in all_db_entries:
+            data = str(data).split("|")
+            self.count = self.count + 1 
+            data_breakdown = {
+                "id" : self.count,
+                "date": data[0],
+                "plate" : data[1]
+            }
+            self.db_data_entries.append(data_breakdown)
 
     def get_owner_data(self,plate):
         return Vehicle.query.filter_by(plate_num = plate).first()
@@ -64,13 +86,10 @@ class DB_Manager():
         return searched_owner[0]["owner_name"] 
 
 
-
-
-
-
 if __name__ == '__main__':
     pass
-    # create db debug
+
+    # ------------------------------ create db debug ----------------------------- #
     # db.create_all()
     # db_man = DB_Manager()
     # db_man.get_db_data()
@@ -81,7 +100,28 @@ if __name__ == '__main__':
     # print(type(db_man.get_owner_data("tk142")))
     # print(type(db_man.db_data))
 
-    # search debug
+    # ------------------------------- search debug ------------------------------- #
     # print(db_man.search_owner("tk142"))
     # test = Vehicle.query.filter_by(plate_num = "plate_num").first()
     # test = Vehicle.query.all()
+
+    # -------------------------------- LOGS DEBUG -------------------------------- #
+
+    # Create Vehicle
+    # car_1 = Vehicle("luh143" , "pinut")
+    # db.session.add(car_1)
+    # db.session.commit()
+
+    # Create Entry
+    # car_1 = Vehicle.query.filter_by(plate_num = "luh143").first()
+    # entry_1 = Entry(entry_vehicle_plate = car_1.plate_num)
+    # db.session.add(entry_1)
+    # db.session.commit()
+
+    # show car entries
+    # car_1 = Vehicle.query.filter_by(plate_num = "luh143").first()
+    # print(f"Car_1 Entries : {car_1.entries}")
+
+    # Show db_man data content
+    # print(db_man.db_data)
+    # print(db_man.db_data_entries)
