3d0fd794e99239511f23e6d19d4249c12c2262d4
Alejandro <shadowslayerhero@gmail.com>
2022-04-23 02:45:43 +0800

    imp: seperate owner div

--- app/static/script.js
+++ app/static/script.js
@@ -1,13 +1,17 @@
 /* ------------------------- SLIDER TOGGLE DETECTION ------------------------ */
 
 $(document).ready(function () {
-      var toggler = $(".toggle-switch");
-      toggler.click(function () {
+    var toggler = $(".toggle-switch");
+
+    toggler.click(function () {
         $(this).toggleClass("active");
+        // $("#videoElement").css("filter" , "blur(2px)"); //Apply blur
         $.getJSON("/request_model_switch", function (data) {
       });
-    });
+      console.log("TEST ON PRESS TOGGLE")
+      // $("#test").hide();
   });
+});
 
 
 /* ------------------------------- DYNAMIC JS ------------------------------- */
@@ -26,9 +30,20 @@ $(function () {
       type: "POST",
       dataType: "json",
       success: function (data) {
+        let dynamic_data = JSON.stringify(data); 
+        
         // console.log("plate ajax success");
-        // console.log(data);
-        document.getElementById("dynamic_plate").innerText = data[1];
+
+        dynamic_data = dynamic_data.split(" | ");
+        console.log(dynamic_data);
+
+        // Manipulate String
+        dynamic_data[0] = dynamic_data[0].substring(5);
+        dynamic_data[1] = dynamic_data[1].slice(0 , -2);
+
+        // Change frontend
+        document.getElementById("dynamic_plate").innerText = dynamic_data[0];
+        document.getElementById("dynamic_owner").innerText =  dynamic_data[1];
       },
     });
   }
@@ -40,14 +55,10 @@ $(function () {
       dataType: "json",
       success: function (data) {
         // console.log("--gate ajax function--");
-        // console.log(data);
-        // document.getElementById("gate-status").innerText = data[1];
         
-        DynamicPlate = document.getElementById("dynamic_plate").innerText
-        DynamicPlate = DynamicPlate.split("| ")[1]
-        // console.log(DynamicPlate); //DEBUG: Check DynamicPlate element val
+        check_owner = document.getElementById("dynamic_owner").innerText
         
-        if (DynamicPlate === "No owner found"){
+        if (check_owner === "No owner found"){
           document.getElementById("gate-status-container").innerText = "Gate Status: Closed";
           document.getElementById("gate-status-container").style.color = "red"
         }
@@ -55,7 +66,6 @@ $(function () {
           document.getElementById("gate-status-container").innerText = "Gate Status: Open";
           document.getElementById("gate-status-container").style.color = "green"
         }
-        
         
       },
     });
